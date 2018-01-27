f9e3e1554175b350bdc158e3f84e7148205ae1c4
AmirMohammad <mohseni.amir1@gmail.com>
2018-01-27 18:05:54 +0330

    reserching not completed

--- src/main/java/massim/javaagents/agents/BasicAgent.java
+++ src/main/java/massim/javaagents/agents/BasicAgent.java
@@ -17,10 +17,12 @@ public class BasicAgent extends Agent {
 
     Double[] researchCoordinates = new Double[2];
 
-    double minLon = 2.26;
+    double minLon = 2.26;//0.15
     double maxLon = 2.41;
-    double minLat = 48.82;
+    double minLat = 48.82;//0.08
     double maxLat = 48.90;
+    double walkLat = 0.008;
+    double walkLon = 0.012;
 
 
     public BasicAgent(String name, MailService mailbox) {
@@ -44,14 +46,17 @@ public class BasicAgent extends Agent {
 
         if (getStepNumber() == 0) {
             sharedData.addMyRole(AP.getSelfInfo().getName(), AP.getSelfRole());
+            researchCoordinates[0] = AP.getSelfInfo().getLat();
+            researchCoordinates[1] = AP.getSelfInfo().getLon();
         }
 
 
-        // TODO if resourceNode is seen save it in shared data
+        // if resourceNode is seen save it in shared data
         if (AP.getResourceNodes().size() != 0) {
             for (resourceNode r : AP.getResourceNodes()) {
                 sharedData.addNewResourceNode(r);
             }
+            System.out.println("!!!! RESOURCE NODE FOUND BY "+AP.getSelfInfo().getName()+" !!!!");
         }
 
         Queue<ArrayList<String>> actions = sharedData.getMyActions(AP.getSelfInfo().getName());
@@ -119,10 +124,10 @@ public class BasicAgent extends Agent {
                 return new Action("deliver_job", pdeliver);
             case "research":
                 // TODO go to a place not visited
-                researchCoordinates = calculateNextResearch();
                 LinkedList<Parameter> parameters = new LinkedList<>();
                 parameters.add(new Identifier(researchCoordinates[0].toString()));
                 parameters.add(new Identifier(researchCoordinates[1].toString()));
+                calculateNextResearch();
                 return new Action("goto", parameters);
             default:
                 return new Action("skip");
@@ -131,11 +136,18 @@ public class BasicAgent extends Agent {
 
     }
 
-    private Double[] calculateNextResearch() {
-        researchCoordinates[0] = AP.getSelfInfo().getLat();
+    private void calculateNextResearch() {
+        researchCoordinates[0] = AP.getSelfInfo().getLat() + walkLat;
         researchCoordinates[1] = AP.getSelfInfo().getLon();
+        if(researchCoordinates[0] > maxLat) {
+            researchCoordinates[0] = maxLat - 0.005;
+            walkLat =  -0.01;
+        }else if(researchCoordinates[0] < minLat){
+            researchCoordinates[0] = minLat + 0.005;
+            walkLat =  0.01;
+        }
+        if(AP.getSelfInfo().getCharge() <= 50){
 
-
-        return null;
+        }
     }
 }
